04cbc2249fedcd7e433d5f922063acb521ef983b
hi-mark <44605927+hi-mark@users.noreply.github.com>
2024-08-01 04:44:22 -0500

    First Commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,37 @@
-import 'home/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "home/styles/globals.css";
+import type { AppProps } from "next/app";
+
+import localFont from "next/font/local";
+
+const NeueMontreal = localFont({
+  src: [
+    {
+      path: "../../public/fonts/NeueMontreal-Regular.otf",
+      weight: "450",
+    },
+    {
+      path: "../../public/fonts/NeueMontreal-Light.otf",
+      weight: "300",
+    },
+    {
+      path: "../../public/fonts/NeueMontreal-Bold.otf",
+      weight: "800",
+    },
+    {
+      path: "../../public/fonts/NeueMontreal-Medium.otf",
+      weight: "530",
+    },
+  ],
+  display: "swap",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div
+      className={NeueMontreal.className}
+      style={{ width: "100% !important" }}
+    >
+      <Component {...pageProps} />
+    </div>
+  );
 }
---
+++ src/utils.ts
@@ -0,0 +1,6 @@
+export const spring = {
+  type: "spring",
+  stiffness: 50,
+  damping: 8,
+  mass: 0.6,
+};
