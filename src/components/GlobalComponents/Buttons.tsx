0b778f4f42093dfb0bfb0a4ae665b06a978df5bd
hi-mark <44605927+hi-mark@users.noreply.github.com>
2024-08-06 01:50:33 -0500

    Added Sign up page layout

---
+++ src/components/GlobalComponents/Buttons.tsx
@@ -0,0 +1,27 @@
+import styles from "home/styles/GlobalComponents/Buttons.module.css";
+
+export const PrimaryButton = (props: any) => {
+  return (
+    <button
+      disabled={props.disabled}
+      className={styles.primaryButton}
+      onClick={props.handleClick}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export const SecondaryButton = (props: any) => {
+  return (
+    <button
+      disabled={props.disabled}
+      className={styles.secondaryButton}
+      onClick={props.handleClick}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default PrimaryButton;
